093937c7d116ef71d012aec5a9d37c487644a48a
Daniel Gilchrist <daniel@workforce.com>
2022-06-15 13:00:05 +0100

    Add some more bindings

--- key_bindings.lua
+++ key_bindings.lua
@@ -19,38 +19,42 @@ local command_alt = combine(command, alt)
 config.keys = {
   keybind(command_shift, "r", "ReloadConfiguration"),
 
-  keybind(command, "d", wezterm.action{SplitHorizontal={
-    domain="CurrentPaneDomain",
+  keybind(command, "d", wezterm.action{SplitHorizontal = {
+    domain = "CurrentPaneDomain",
   }}),
 
-  keybind(command_shift, "d", wezterm.action{SplitVertical={
-    domain="CurrentPaneDomain",
+  keybind(command_shift, "d", wezterm.action{SplitVertical = {
+    domain = "CurrentPaneDomain",
   }}),
 
-  keybind(command, "k", wezterm.action{ClearScrollback="ScrollbackAndViewport"}),
+  keybind(command, "k", wezterm.action{ClearScrollback = "ScrollbackAndViewport"}),
 
   keybind(command, "w", wezterm.action{
-    CloseCurrentPane={
+    CloseCurrentPane = {
       confirm = false,
     }
   }),
 
   keybind(command_shift, "w", wezterm.action{
-    CloseCurrentTab={
+    CloseCurrentTab = {
       confirm = true,
     }
   }),
 
-  keybind(command_alt, "LeftArrow", wezterm.action{ActivatePaneDirection="Left"}),
-  keybind(command_alt, "RightArrow", wezterm.action{ActivatePaneDirection="Right"}),
-  keybind(command_alt, "UpArrow", wezterm.action{ActivatePaneDirection="Up"}),
-  keybind(command_alt, "DownArrow", wezterm.action{ActivatePaneDirection="Down"}),
+  keybind(command_alt, "LeftArrow", wezterm.action{ActivatePaneDirection = "Left"}),
+  keybind(command_alt, "RightArrow", wezterm.action{ActivatePaneDirection = "Right"}),
+  keybind(command_alt, "UpArrow", wezterm.action{ActivatePaneDirection = "Up"}),
+  keybind(command_alt, "DownArrow", wezterm.action{ActivatePaneDirection = "Down"}),
 
-  keybind(command, "LeftArrow", wezterm.action{ActivateTabRelative=-1}),
-  keybind(command, "RightArrow", wezterm.action{ActivateTabRelative=1}),
+  keybind(command, "LeftArrow", wezterm.action{ActivateTabRelative = -1}),
+  keybind(command, "RightArrow", wezterm.action{ActivateTabRelative = 1}),
 
   keybind(command_shift, "UpArrow", "ScrollToTop"),
   keybind(command_shift, "DownArrow", "ScrollToBottom"),
+  keybind(alt, "PageUp", wezterm.action{ScrollByPage = -1}),
+  keybind(command_alt, "PageUp", wezterm.action{ScrollByPage = -6}),
+  keybind(alt, "PageDown", wezterm.action{ScrollByPage = 1}),
+  keybind(command_alt, "PageDown", wezterm.action{ScrollByPage = 6}),
 
   keybind("CMD", "Enter", "ToggleFullScreen")
 }
