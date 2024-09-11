f7a5159239ad6119e650b5e6cde7828b3c4cd91f
Daniel Gilchrist <daniel@workforce.com>
2024-09-11 15:04:24 +0100

    Add binding for new window

--- key_bindings.lua
+++ key_bindings.lua
@@ -1,12 +1,12 @@
-local wezterm = require "wezterm"
-local key_utils = require "util.key".key_utils
+local wezterm = require("wezterm")
+local key_utils = require("util.key").key_utils
 
 local function keybind(mods, key, action)
-  return {mods = mods, key = key, action = action}
+	return { mods = mods, key = key, action = action }
 end
 
 local function combine(key1, key2)
-  return key1 .. "|" .. key2
+	return key1 .. "|" .. key2
 end
 
 local config = {}
@@ -20,52 +20,70 @@ local command_alt = combine(command, alt)
 local shift_alt = combine(shift, alt)
 
 config.keys = {
-  keybind(command_shift, "r", "ReloadConfiguration"),
+	keybind(command_shift, "r", "ReloadConfiguration"),
 
-  keybind(command, "d", wezterm.action{SplitHorizontal = {
-    domain = "CurrentPaneDomain",
-  }}),
+	keybind(command_shift, "n", wezterm.action.SpawnWindow),
 
-  keybind(command_shift, "d", wezterm.action{SplitVertical = {
-    domain = "CurrentPaneDomain",
-  }}),
+	keybind(
+		command,
+		"d",
+		wezterm.action({ SplitHorizontal = {
+			domain = "CurrentPaneDomain",
+		} })
+	),
 
-  keybind(command, "k", wezterm.action{ClearScrollback = "ScrollbackAndViewport"}),
+	keybind(
+		command_shift,
+		"d",
+		wezterm.action({ SplitVertical = {
+			domain = "CurrentPaneDomain",
+		} })
+	),
 
-  keybind(command, "w", wezterm.action{
-    CloseCurrentPane = {
-      confirm = false,
-    }
-  }),
+	keybind(command, "k", wezterm.action({ ClearScrollback = "ScrollbackAndViewport" })),
 
-  keybind(command_shift, "w", wezterm.action{
-    CloseCurrentTab = {
-      confirm = true,
-    }
-  }),
+	keybind(
+		command,
+		"w",
+		wezterm.action({
+			CloseCurrentPane = {
+				confirm = false,
+			},
+		})
+	),
 
-  keybind(command_alt, "LeftArrow", wezterm.action{ActivatePaneDirection = "Left"}),
-  keybind(command_alt, "RightArrow", wezterm.action{ActivatePaneDirection = "Right"}),
-  keybind(command_alt, "UpArrow", wezterm.action{ActivatePaneDirection = "Up"}),
-  keybind(command_alt, "DownArrow", wezterm.action{ActivatePaneDirection = "Down"}),
+	keybind(
+		command_shift,
+		"w",
+		wezterm.action({
+			CloseCurrentTab = {
+				confirm = true,
+			},
+		})
+	),
 
-  keybind(command, "LeftArrow", wezterm.action{ActivateTabRelative = -1}),
-  keybind(command, "RightArrow", wezterm.action{ActivateTabRelative = 1}),
+	keybind(command_alt, "LeftArrow", wezterm.action({ ActivatePaneDirection = "Left" })),
+	keybind(command_alt, "RightArrow", wezterm.action({ ActivatePaneDirection = "Right" })),
+	keybind(command_alt, "UpArrow", wezterm.action({ ActivatePaneDirection = "Up" })),
+	keybind(command_alt, "DownArrow", wezterm.action({ ActivatePaneDirection = "Down" })),
 
-  keybind(command_shift, "UpArrow", "ScrollToTop"),
-  keybind(command_shift, "DownArrow", "ScrollToBottom"),
-  keybind(alt, "PageUp", wezterm.action{ScrollByPage = -1}),
-  keybind(command_alt, "PageUp", wezterm.action{ScrollByPage = -6}),
-  keybind(alt, "PageDown", wezterm.action{ScrollByPage = 1}),
-  keybind(command_alt, "PageDown", wezterm.action{ScrollByPage = 6}),
+	keybind(command, "LeftArrow", wezterm.action({ ActivateTabRelative = -1 })),
+	keybind(command, "RightArrow", wezterm.action({ ActivateTabRelative = 1 })),
 
-  keybind(shift_alt, "{", wezterm.action{MoveTabRelative = -1}),
-  keybind(shift_alt, "}", wezterm.action{MoveTabRelative = 1}),
+	keybind(command_shift, "UpArrow", "ScrollToTop"),
+	keybind(command_shift, "DownArrow", "ScrollToBottom"),
+	keybind(alt, "PageUp", wezterm.action({ ScrollByPage = -1 })),
+	keybind(command_alt, "PageUp", wezterm.action({ ScrollByPage = -6 })),
+	keybind(alt, "PageDown", wezterm.action({ ScrollByPage = 1 })),
+	keybind(command_alt, "PageDown", wezterm.action({ ScrollByPage = 6 })),
 
-  keybind(command_shift, "m", wezterm.action{PaneSelect = {mode = "SwapWithActive"}}),
-  keybind(command_shift, "p", wezterm.action{PaneSelect = {mode = "Activate"}}),
+	keybind(shift_alt, "{", wezterm.action({ MoveTabRelative = -1 })),
+	keybind(shift_alt, "}", wezterm.action({ MoveTabRelative = 1 })),
 
-  keybind(command, enter, "ToggleFullScreen"),
+	keybind(command_shift, "m", wezterm.action({ PaneSelect = { mode = "SwapWithActive" } })),
+	keybind(command_shift, "p", wezterm.action({ PaneSelect = { mode = "Activate" } })),
+
+	keybind(command, enter, "ToggleFullScreen"),
 }
 
 return config
